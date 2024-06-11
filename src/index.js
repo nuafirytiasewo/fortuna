d199e633170174c682750c952d03b44c36df16ad
Dima M <devontelnel@mail.ru>
2024-06-11 14:31:41 +0400

    теперь уже есть что то похожее на колесо фортуны: линии крутятся вокруг центра в окружности, образуя крутящиеся сегменты

--- src/index.js
+++ src/index.js
@@ -25,14 +25,13 @@ console.log("Размеры окна: " + app.screen.width + " " + app.screen.he
 console.log("Координаты контейнера: " + containerMain.x + " " + containerMain.y);
 
 
-
 //определяем диаметр колеса (ширина жкрана / 3)
 const diameterWheelBase = app.screen.width / 3;
+const radiusWheelBase = diameterWheelBase / 2;
 //вывод
 console.log("Диаметр основного колеса: " + diameterWheelBase);
 // толщина линии и колеса (диаметр колеса / 140)
 const borderWheel = diameterWheelBase / 140;
-
 
 
 //основа для колеса
@@ -42,7 +41,7 @@ wheelBase.lineStyle(borderWheel, 0xfeeb77, 1);
 // заполнение(цвет,прозрачность)
 wheelBase.beginFill(0x650a5a, 1);
 //рисование круга(коорд x коорд y)
-wheelBase.drawCircle(0, 0, diameterWheelBase / 2);
+wheelBase.drawCircle(0, 0, radiusWheelBase);
 //заканчиваем заполнение
 wheelBase.endFill();
 
@@ -52,17 +51,31 @@ wheelBase.y = containerMain.height / 2;
 console.log("Координаты основного колеса: " + wheelBase.x + " " + wheelBase.y);
 console.log("Размер основного колеса: " + wheelBase.width + " " + wheelBase.height);
 
-//линия, разделяющая сегменты
-const lineSegment = new PIXI.Graphics();
-// граница линии (толщина, цвет, прозрачность)
-lineSegment.lineStyle(borderWheel, 0xfeeb77);
-// где линия начинается (относительно колеса, по x в 0, а по y - в точке, значение которой равно радиус колеса)
-lineSegment.moveTo(0, diameterWheelBase / 2);
-// где линия заканчивается
-lineSegment.lineTo(0, 0);
-//заканчиваем заполнение
-lineSegment.endFill();
 
+let amountSegments = 4 // количество сегментов
+let angelStep = (2 * Math.PI)/amountSegments //вычисляем угол сегментов в радианах (постоянная)
+
+for (let i = 0; i < amountSegments; i++)
+{
+    //линия, разделяющая сегменты
+    const lineSegment = new PIXI.Graphics();
+    // граница линии (толщина, цвет, прозрачность)
+    lineSegment.lineStyle(borderWheel, 0xfeeb77);
+    // умножаем угол с каждой итерацией (секментом) на величину угла между ними
+    let angel = angelStep * i
+    //координаты для того чтобы вычислить где будет точка на окружности колеса 
+    let xLine = radiusWheelBase * Math.cos(angel)
+    let yLine = radiusWheelBase * Math.sin(angel)
+    // начало или конец линии 
+    lineSegment.moveTo(xLine, yLine);
+    // конец или начало линии (в середине колеса)
+    lineSegment.lineTo(0, 0);
+    //заканчиваем заполнение
+    lineSegment.endFill();
+
+    //добавляем линию на основу для колеса
+    wheelBase.addChild(lineSegment);
+}
 
 
 //круг в центре (кнопка)
@@ -72,21 +85,15 @@ wheelButton.lineStyle(0);
 // заполнение(цвет,прозрачность)
 wheelButton.beginFill(0xde3249, 1);
 //рисование круга(коорд x коорд y)
-wheelButton.drawCircle(0, 0, (diameterWheelBase / 2) / 10);
+wheelButton.drawCircle(0, 0, radiusWheelBase / 10);
 //заканчиваем заполнение
 wheelButton.endFill();
 
 
-
-//добавляем линию на основу для колеса
-wheelBase.addChild(lineSegment);
-
 //добавляем основу для колеса
 containerMain.addChild(wheelBase);
-
 //добавляем круг на основу для колеса
 containerMain.addChild(wheelButton);
-
 //добавляем контейнер на сцену
 app.stage.addChild(containerMain);
 
